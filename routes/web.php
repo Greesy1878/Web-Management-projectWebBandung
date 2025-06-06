7410c8470343b1ed18208bffdf18e1d9672ebb26
Greesy1878 <rajagreesy@gmail.com>
2025-06-07 01:32:08 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CMSController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PariwisataController;
+use App\Http\Controllers\UMKMController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/admin', [DashboardController::class, 'index'])
@@ -13,3 +14,7 @@ Route::get('/admin', [DashboardController::class, 'index'])
 
     Route::get('/pariwisata', [PariwisataController::class, 'index'])
         ->name('pariwisata.dashboard');
+
+    Route::get('/umkm', [UMKMController::class, 'index'])
+        ->name('umkm.dashboard');
+    
